7c00c613a04da4049cf2742d4816496badc0b3ce
Jianshu-Hu <jianshuhu98@gmail.com>
2022-10-26 17:03:45 +0800

    update shell file for running experiments

--- run_experiments/run_cluster.sh
+++ run_experiments/run_cluster.sh
@@ -4,8 +4,8 @@ cd /bigdata/users/jhu/hidden-drq
 source /bigdata/users/yjiang/miniconda3/bin/activate
 conda activate drq
 
-tag=drq_with_attention
+tag=drq
 seed=1
 
 echo "start running $tag with seed $seed"
-python train.py CBAM=true regularization=1 env=cartpole_swingup batch_size=512 action_repeat=8 tag=$tag seed=$seed
+python train.py CBAM=false regularization=1 env=cartpole_swingup batch_size=512 action_repeat=8 num_train_steps=100000 tag=$tag seed=$seed
