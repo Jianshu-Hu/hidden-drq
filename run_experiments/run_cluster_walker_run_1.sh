59ff47cf9befeaa9bf30e8fdbff79071778ee657
Jianshu-Hu <jianshuhu98@gmail.com>
2023-01-16 12:04:26 +0800

    basic modification for running experiments on cluster

---
+++ run_experiments/run_cluster_walker_run_1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /bigdata/users/jhu/hidden-drq
+source /bigdata/users/yjiang/miniconda3/bin/activate
+conda activate drq
+
+tag=drq
+seed=1
+
+echo "start running $tag with seed $seed"
+python train.py env=walker_run batch_size=512 action_repeat=2 num_train_steps=250000 tag=$tag seed=$seed
