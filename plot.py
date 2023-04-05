86243ba9cfaf337a20c07948233fc08c58906918
Jianshu-Hu <jianshuhu98@gmail.com>
2023-04-05 11:37:21 +0800

    update plot

--- plot.py
+++ plot.py
@@ -323,11 +323,14 @@ folders_all = ['rad', 'drq+not_avg_target', 'drq+avg_target', 'drq+avg_target+01
                'drq+avg_target+01_kl+01_tangent_prop']
 
 
-folders_42 = ['drq+avg_target', 'drq+avg_target+09_beta_dist', 'drq+avg_target+08_beta_dist',
-              'drq+avg_target+07_beta_dist', 'drq+avg_target+06_beta_dist', 'drq+avg_target+05_scheduled_beta_dist']
-folders_43 = ['drq+avg_target', 'drq+avg_target+07_beta_dist', 'drq+avg_target+08_beta_dist',
-              'drq+avg_target+scheduled_07_beta_dist',
-              'drq+avg_target+scheduled_07_2_beta_dist', 'drq+avg_target+scheduled_1_2_beta_dist']
+# folders_42 = ['drq+avg_target', 'drq+avg_target+09_beta_dist', 'drq+avg_target+08_beta_dist',
+#               'drq+avg_target+07_beta_dist', 'drq+avg_target+06_beta_dist', 'drq+avg_target+05_scheduled_beta_dist']
+# folders_43 = ['drq+avg_target', 'drq+avg_target+07_beta_dist', 'drq+avg_target+08_beta_dist',
+#               'drq+avg_target+scheduled_07_beta_dist',
+#               'drq+avg_target+scheduled_07_2_beta_dist', 'drq+avg_target+scheduled_1_2_beta_dist']
+folders_42 = ['drq+avg_target+01_kl', 'drq+avg_target+01_kl+trainable_dist']
+folders_43 = ['drq+avg_target+01_kl', 'drq+avg_target+01_kl+trainable_dist',
+              'drq+avg_target+01_kl+trainable_dist_smaller_lr']
 folders_44 = ['drq+avg_target', 'drq+avg_target+1_kl', 'drq+avg_target+05_kl',
               'drq+avg_target+01_kl']
 folders_45 = ['drq+avg_target', 'drq+avg_target+01_kl', 'drq+avg_target+01_kl+scheduled_07_beta_dist',
@@ -350,10 +353,15 @@ plot_several_folders(prefix=prefix_3, folders=folders_all, title='reacher_hard_a
 plot_several_folders(prefix=prefix_7, folders=folders_all, title='walker_run_all')
 plot_several_folders(prefix=prefix_9, folders=folders_all, title='finger_spin_all')
 
-plot_several_folders(prefix=prefix_2, folders=folders_42, title='cheetah_run_beta_dist')
-plot_several_folders(prefix=prefix_3, folders=folders_beta_dist, title='reacher_hard_beta_dist')
-plot_several_folders(prefix=prefix_7, folders=folders_43, title='walker_run_beta_dist')
-plot_several_folders(prefix=prefix_9, folders=folders_beta_dist, title='finger_spin_beta_dist')
+# plot_several_folders(prefix=prefix_2, folders=folders_42, title='cheetah_run_beta_dist')
+# plot_several_folders(prefix=prefix_3, folders=folders_beta_dist, title='reacher_hard_beta_dist')
+# plot_several_folders(prefix=prefix_7, folders=folders_43, title='walker_run_beta_dist')
+# plot_several_folders(prefix=prefix_9, folders=folders_beta_dist, title='finger_spin_beta_dist')
+
+# plot_several_folders(prefix=prefix_2, folders=folders_42, title='cheetah_run_beta_dist_new')
+# plot_several_folders(prefix=prefix_3, folders=folders_beta_dist, title='reacher_hard_beta_dist')
+plot_several_folders(prefix=prefix_7, folders=folders_43, title='walker_run_beta_dist_new')
+# plot_several_folders(prefix=prefix_9, folders=folders_beta_dist, title='finger_spin_beta_dist')
 
 plot_several_folders(prefix=prefix_2, folders=folders_46, title='cheetah_run_kl')
 plot_several_folders(prefix=prefix_3, folders=folders_44, title='reacher_hard_kl')
