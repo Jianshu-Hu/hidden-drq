d89e65c3394ceb9100d2645b2519b69bd086ee1a
Jianshu-Hu <jianshuhu98@gmail.com>
2023-04-23 10:47:33 +0800

    fix a small typo

--- run_experiments/run_cluster_pendulum_swingup_tangent_2.sh
+++ run_experiments/run_cluster_pendulum_swingup_tangent_2.sh
@@ -8,4 +8,4 @@ tag=pendulum_swingup+crop+DrQ_avg_target+01_kl+01_q_tan
 seed=2
 
 echo "start running $tag with seed $seed"
-python train.py data_aug=1 add_kl_loss=true add_q_tan=true visualize=true env=pendulum_swingup action_repeat=2 num_train_steps=250000 tag=$tag seed=$seed
+python train.py data_aug=1 add_kl_loss=true q_tan_prop=true visualize=true env=pendulum_swingup action_repeat=2 num_train_steps=250000 tag=$tag seed=$seed
